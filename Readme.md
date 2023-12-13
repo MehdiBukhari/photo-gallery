9baf81460728d2c520c4812ece0bb65e74376625
Mehdi Bukhari <mmmbukhari@gmail.com>
2023-12-13 13:09:43 +0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,100 @@
+
+
+```markdown
+# Photo Gallery
+
+This is a simple photo gallery app built using Ionic and Capacitor.
+
+## Features
+
+- View a collection of photos
+- Upload new photos
+- Swipe through photos
+- Share photos with friends
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- Node.js and npm
+- Ionic CLI
+- Capacitor
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone <Get URL Above.>
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd photo-gallery
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Add your platform:
+
+   ```bash
+   ionic capacitor add android
+   ```
+
+   or
+
+   ```bash
+   ionic capacitor add ios
+   ```
+
+## Usage
+
+1. Run the app in the browser:
+
+   ```bash
+   ionic serve
+   ```
+
+2. To build and open on a specific platform:
+
+   For Android:
+
+   ```bash
+   ionic capacitor run android
+   ```
+
+   For iOS:
+
+   ```bash
+   ionic capacitor run ios
+   ```
+
+## Contributing
+
+If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the repository
+2. Create a new branch: `git checkout -b feature/your-feature`
+3. Make your changes and commit them: `git commit -m 'Add some feature'`
+4. Push to the branch: `git push origin feature/your-feature`
+5. Create a pull request
+
+## License
+
+This project is licensed under the MIT License - 
+
+## Acknowledgments
+
+- This app was built with Ionic and Capacitor.
+
+## Contact
+
+If you have any questions or feedback, feel free to contact us at mmmbukhari@gmail.com.
+
+````
+ Happy coding!
---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo  } from '@capacitor/camera';
+import { Capacitor } from '@capacitor/core';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { Platform  } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+ public photos: UserPhoto[] = [];
+ private PHOTO_STORAGE: string = "photos";
+ private platform: Platform;
+  constructor(platform: Platform) {
+    this.platform = platform;
+   }
+  public async addNewToGallery(){
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+    const savedImageFile = await this.savePicture(capturedPhoto)
+    this.photos.unshift(savedImageFile)
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
+    })
+  }
+  private async savePicture(photo: Photo){
+
+    const base64Data = await this.readAsBase64(photo);
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    })
+    if(this.platform.is('hybrid')){
+      return {
+        filepath: savedFile.uri,
+        webviewpath: Capacitor.convertFileSrc(savedFile.uri)
+      }
+    }else{
+      return {
+        filepath: fileName,
+        webviewpath: photo.webPath!
+      }
+    }
+
+
+  }
+  private async readAsBase64(photo: Photo){
+    // Featch the photo, read as a blob, then convert to base64 formate
+    if(this.platform.is('hybrid')){
+      const file = await Filesystem.readFile({
+        path: photo.path!
+      })
+      return file.data
+    }
+    else {
+      const response = await fetch(photo.webPath!)
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
+
+  }
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resovle, reject)=>{
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () =>{
+        resovle(reader.result);
+      };
+      reader.readAsDataURL(blob);
+  })
+  public async loadSaved() {
+    const { value } = await Preferences.get({key: this.PHOTO_STORAGE})
+    this.photos = (value ? JSON.parse(value): [] as UserPhoto[]);
+    if(!this.platform.is('hybrid')){
+      for (let photo of this.photos){
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data
+        });
+
+        photo.webviewpath = `data:image/jpeg;base64,${readFile.data}`
+      }
+    }
+
+  }
+}
+
+export interface UserPhoto{
+  filepath: string;
+  webviewpath: string;
+}
